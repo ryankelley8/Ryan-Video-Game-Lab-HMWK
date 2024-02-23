c5f01916c5bcc3d71e50d97da6df11070142dfcc
ryankelley8 <157061728+ryankelley8@users.noreply.github.com>
2024-02-23 03:35:15 -0500

    Animated Sprite

---
+++ Animated Sprite/Sheep.gd
@@ -0,0 +1,13 @@
+extends AnimatedSprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_pressed("ui_right"):
+		position.x +=1
+		play("Happy Sheep All")
